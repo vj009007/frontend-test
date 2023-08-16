518e20db34b71d06e5a4c9452e32afef8fa00ad8
Md-Rizwan <rizwan@saglus.com>
2023-08-16 15:47:40 +0530

    Icons Updates

--- pages/aside-Nav.tsx
+++ pages/aside-Nav.tsx
@@ -1,5 +1,11 @@
 import { Box, Link } from "@mui/material";
 import router, { useRouter } from "next/router";
+import Image from "next/image";
+import gridPic from "../public/grid.svg";
+import BnakPic from "../public/bank.svg";
+import orderPic from "../public/cart.svg";
+import walletPic from "../public/wallet.svg";
+import ChartPic from "../public/clipboard-data.svg";
 
 export default function NewNav() {
 
@@ -11,19 +17,24 @@ export default function NewNav() {
       <Box className="aside-nav max-w-[246px] w-full">
         <ul className="menu-list">
           <li className={router.pathname == "/" ? "active" : ""}>
-            <Link href="/">Dashboard</Link>
+            <Link href="/">
+            <Image src={gridPic} alt="Grid" />  Dashboard</Link>
           </li>
           <li className={router.pathname == "/marketplace" ? "active" : ""}>
-            <Link href="/marketplace">Marketplace</Link>
+            <Link href="/marketplace">
+            <Image src={BnakPic} alt="BnakPic" /> Marketplace</Link>
           </li>
           <li className={router.pathname == "/order" ? "active" : ""}>
-            <Link href="/order">Order</Link>
+            <Link href="/order">
+            <Image src={orderPic} alt="orderPic" /> Order</Link>
           </li>
           <li className={router.pathname == "/payment" ? "active" : ""}>
-            <Link href="/payment">Payment & Invoice</Link>
+            <Link href="/payment">
+            <Image src={walletPic} alt="walletPic" />   Payment & Invoice</Link>
           </li>
           <li className={router.pathname == "/pricing" ? "active" : ""}>
-            <Link href="/pricing">Live Pricing</Link>
+            <Link href="/pricing">
+            <Image src={ChartPic} alt="ChartPic" /> Live Pricing</Link>
           </li>
         </ul>
       </Box>
